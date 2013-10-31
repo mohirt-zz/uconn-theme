4a117b2ac4fc0cdeb9d81a177b66c5946e8e0625
vagrant <vagrant@morganhi-vagrant-i7latest.23456.i7latest.dgicloud.com>
2013-10-31 15:01:29 +0000

    IE conditional styles

---
+++ templates/islandora-book.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Template file to style output.
+ */
+?>
+<?php dsm('frak off'); ?>
+<a href="www.google.ca">Download</a>
+<?php if (isset($viewer)): ?>
+  <div id="book-viewer">
+    <?php print $viewer; ?>
+  </div>
+<?php endif; ?>
+<!-- @todo Add table of metadata values -->
+
